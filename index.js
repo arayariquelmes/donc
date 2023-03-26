e2a4e6d6a28c3c269cc95589c161c0d4b6ebfda5
Sebastián Araya <arayariquelmes@gmail.com>
2023-03-26 20:41:27 -0300

    Inicial

---
+++ index.js
@@ -0,0 +1,6 @@
+import {getAll, CANTIDAD_PERSONAJES} from './services/characters-service.js';
+
+
+getAll();
+
+CANTIDAD_PERSONAJES(1);
---
+++ services/characters-service.js
@@ -0,0 +1,14 @@
+
+function getAll(){
+    console.log("llamar a funcion de rick y morty");
+}
+
+function get(id){
+    console.log("esto recibe un id");
+}
+
+let CANTIDAD_PERSONAJES =20;
+
+CANTIDAD_PERSONAJES = get;
+
+export {getAll,get, CANTIDAD_PERSONAJES};

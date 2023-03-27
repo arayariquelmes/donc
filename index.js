4b77d10a9713beeb6c561e3018d8c34f9f939490
Sebastián Araya <arayariquelmes@gmail.com>
2023-03-26 22:49:08 -0300

    Ejemplo promesas

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
+import bootstrap from './bootstrap.js';
 import {getAll, CANTIDAD_PERSONAJES} from './services/characters-service.js';
 
+const main = async()=>{
+    console.log("Aqui partio la custion");
 
-getAll();
+    try{
+        const promesa = getAll();
+        const res = await promesa;
+        /*promesa.then((res)=>{
 
-CANTIDAD_PERSONAJES(1);
+  
+         }) */
+    
+        console.log(`La promesa se resolvio papu`);
+        let personajes = res;
+        console.log("cantidad de personajes " + personajes.length);
+        console.log("Esto se esta ejecutando despues de la promesa pero va a salir antes");
+    }catch(e){
+        console.log(`error papu ${e}`);
+    }
+}
+
+const funcionHija =  async()=>{
+    console.log(`Aqui parte la aplicacion`);
+    //main().then((r)=>{
+    //    console.log(`aqui finaliza la app`);
+    //});
+    await main();
+    console.log(`aqui finaliza la app`);
+}
+
+bootstrap();
+funcionHija();
+
+//let res = getAll();
+//console.log(res);
+//console.log(process.env);
+//CANTIDAD_PERSONAJES(1);c
--- services/characters-service.js
+++ services/characters-service.js
@@ -1,14 +1,32 @@
+import axios from "axios";
 
-function getAll(){
-    console.log("llamar a funcion de rick y morty");
+const getAll = async()=>{
+    const url = process.env.URL_CHARACTERS;
+    //console.log(`llamar a funcion de rick y morty ${url}`);
+    try{
+        let resultado = await axios.get(url);
+
+        return resultado.data.results;
+    }catch(err){
+        throw new Error(`se fue a la b cabros`);
+    }
+    /*let promesa = new Promise((cuandoFuncione, reject)=>{
+        resultado.then(res=>{
+            cuandoFuncione(res.data.results);
+        }).catch(err=>{
+            reject(`se fue a la b cabros`);
+        });
+    });
+
+    return promesa;
+    */
+
 }
 
 function get(id){
     console.log("esto recibe un id");
 }
-
 let CANTIDAD_PERSONAJES =20;
-
 CANTIDAD_PERSONAJES = get;
 
 export {getAll,get, CANTIDAD_PERSONAJES};
